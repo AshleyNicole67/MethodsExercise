0ecf60ea1aa017f05aaf4c816e48af51b3636f41
Ashley Doering <Ashley.doering@outlook.com>
2026-06-14 21:41:34 -0400

    Created a code to tell a story using ReadLine and WriteLine within the method Madlib

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -2,9 +2,22 @@
 {
     public class Program
     {
+        public static void Madlib()
+        {
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Hello, {userName}! What is your favorite color?");
+            string favColor = Console.ReadLine();
+            Console.WriteLine($"Me too {userName}! What is your favorite animal?");
+            string animal = Console.ReadLine();
+            Console.WriteLine($"Ha Ha, {animal} that's a cool animal! What is your favorite band?");
+            string band = Console.ReadLine();
+            
+            Console.WriteLine ($"Once upon a time, {userName} was playing at the park when they saw a {animal} holding a {favColor} Ballon, listening to {band} while skipping down the street!");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Madlib();
         }
     }
 }
