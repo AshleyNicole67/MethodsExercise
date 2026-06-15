717d9d5a0301e4e56636823c57ee239ec3302a02
Ashley Doering <Ashley.doering@outlook.com>
2026-06-14 22:10:13 -0400

    created code using addition, subtraction, multiplication and division.

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -15,9 +15,38 @@
             
             Console.WriteLine ($"Once upon a time, {userName} was playing at the park when they saw a {animal} holding a {favColor} Ballon, listening to {band} while skipping down the street!");
         }
+
+        public static int Add(int numOne, int numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        public static int Subtract(int numOne, int numTwo)
+        {
+            return numOne - numTwo;
+        }
+
+        public static int Multiply(int numOne, int numTwo)
+        {
+            return numOne * numTwo;
+        }
+
+        public static int Divide(int numOne, int numTwo)
+        {
+            return numOne / numTwo;
+        }
+
+
+        
+        
         static void Main(string[] args)
         {
-            Madlib();
+            //Madlib();
+            //Console.WriteLine(Add(numOne:2, numTwo:2));
+            //Console.WriteLine(Subtract(2, 2));
+            //Console.WriteLine(Multiply(2, 2));
+            //Console.WriteLine(Divide(2, 2));
+
         }
     }
 }
